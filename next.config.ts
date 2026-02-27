bb9b0e0e30513a645f16004779e06a982334b5ac
ndatuangc02 <148603047+ndatuangc02@users.noreply.github.com>
2026-02-27 17:29:55 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,9 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+}
+
+module.exports = nextConfig
